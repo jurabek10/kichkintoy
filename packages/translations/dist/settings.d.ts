b151795e387f3dfbdfd2d882978c1c2faf339d6e
jurabek10 <samiev.jurabek@bk.ru>
2026-06-13 01:27:56 +0900

    Add translations workspace package

---
+++ packages/translations/dist/settings.d.ts
@@ -0,0 +1,7 @@
+export declare const fallbackLng = "uz";
+export declare const languages: readonly ["uz", "en", "ru"];
+export type Language = (typeof languages)[number];
+export declare const defaultNS = "common";
+export declare const cookieName = "i18next";
+export declare function isSupportedLanguage(language: string): language is Language;
+//# sourceMappingURL=settings.d.ts.map
---
+++ packages/translations/dist/settings.js
@@ -0,0 +1,8 @@
+export const fallbackLng = "uz";
+export const languages = [fallbackLng, "en", "ru"];
+export const defaultNS = "common";
+export const cookieName = "i18next";
+export function isSupportedLanguage(language) {
+    return languages.includes(language);
+}
+//# sourceMappingURL=settings.js.map
