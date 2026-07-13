3dd7e34a4eff89df97752f638ae2ad12d21846f0
jurabek10 <samiev.jurabek@bk.ru>
2026-07-13 23:48:35 +0900

    chore(db): reconcile complaint id defaults

---
+++ packages/api/prisma/migrations/20260713143417_comments_media/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "complaint_replies" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "complaint_status_events" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "complaints" ALTER COLUMN "id" DROP DEFAULT;
